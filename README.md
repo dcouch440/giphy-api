777c6d039f3bc2b853446c2163e3dcb27417482c
dcouch440 <dcouch440@gmail.com>
2021-02-09 13:40:38 -0800

    refactor api call with promise
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 #### *Co Created By: David Couch*
 #### *Co Created By: Max Meyer*
 #### *Co Created By: Jon Stump*
+#### *Co Created By: Louie Schoenknecht*
+
 
 #### *This is api practice for school.*
 * * *
@@ -13,7 +15,7 @@ This is a non functional/finalized project made to test api calls. This projects
 * Created on 2/8/21
 
 ## License:
-> *&copy; David Couch, Jon Stump, Max Meyer 2021*
+> *&copy; David Couch, Jon Stump, Max Meyer, Louie Schoenknecht 2021*
 
 Licensed under [MIT license](https://mit-license.org/)
 
@@ -23,3 +25,4 @@ Licensed under [MIT license](https://mit-license.org/)
 _David Couch: [Email](dcouch440@gmail.com)_
 _Jon Stump: [Email](jmstump@gmail.com)_
 _Maxwell Meyer: [Email](maxreadswell@gmail.com)_
+_Louie Schoenknecht: [Email](luisesch97@gmail.com)_
--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,63 @@
 import '../css/styles.css';
 import $ from 'jquery';
 
-$(document).ready(function() {
-  const key = process.env.API_KEY;
-
-  $('#buttons').click(function(event) {
-    const input = $('#keyword').val();
+class GiphyService {
+  static getGif(currentId, query) {
+    const key = process.env.API_KEY;
     let request = new XMLHttpRequest();
-    let {id} = event.target;
-    let url;
-
-    switch(id) {
-    case "search":
-      url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${input}&limit=5`;
-      break;
-    case "trending":
-      url = `https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=5`;
-      break;
-    case "random":
-      url = `https://api.giphy.com/v1/gifs/random?api_key=${key}&limit=5`;
-      break;
-    }
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        Array.isArray(response.data) ? 
-          gifMapper(response):
-          getRandom(response);
+    return new Promise(function(resolve, reject) {
+      let url;
+      switch(currentId) {
+      case "search":
+        url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${query}&limit=5`;
+        break;
+      case "trending":
+        url = `https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=5`;
+        break;
+      case "random":
+        url = `https://api.giphy.com/v1/gifs/random?api_key=${key}&limit=5`;
+        break;
       }
-    };
+      
+      request.onreadystatechange = function() {
+        if (request.readyState !== 4) return;
+        if (request.status >= 200 && request.status < 300) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject({
+            status: request.status,
+            statusText: request.statusText
+          });
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+      
+  }
+}
+function gifMapper(response) {
+  const data = response.data.map(myObj => `<img class="response" src="${myObj.images.original.url}">`).join('');
+  $('.showGif').html(data);
+}
 
-    request.open("GET", url, true);
-    request.send();
+function getRandom(response) {
+  const data = `<img class="response" src="${response.data.images.original.url}">`;
+  $('.showGif').html(data);
+}
 
-    function gifMapper(response) {
-      const data = response.data.map(myObj => `<img class="response" src="${myObj.images.original.url}">`).join('');
-      $('.showGif').html(data);
-    }
-    function getRandom(response) {
-      const data = `<img class="response" src="${response.data.images.original.url}">`;
-      $('.showGif').html(data);
-    }
+$(document).ready(function() {
+  $('#buttons').click(function(event) {
+    event.preventDefault();
+    const input = $('#keyword').val();
+    let {id} = event.target;
+    GiphyService.getGif(id, input).then(response => {
+      Array.isArray(response.data) ? 
+        gifMapper(response):
+        getRandom(response);
+    }).catch(oopsie => {
+      console.log(oopsie);
+    });
   });
 });
 
